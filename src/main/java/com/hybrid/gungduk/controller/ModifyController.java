74a968e57de3c12d7ae32db2112ad1622bb565db
서현아 <@DESKTOP-LS2IQPE>
2018-09-08 14:47:33 +0900

    회원정보 수정 형식 수정

--- src/main/java/com/hybrid/gungduk/controller/ModifyController.java
+++ src/main/java/com/hybrid/gungduk/controller/ModifyController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +17,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import com.hybrid.gungduk.dao.ModifyDao;
 import com.hybrid.gungduk.dto.LoginDto;
 import com.hybrid.gungduk.dto.UserDto;
+import com.hybrid.gungduk.dto.ValPwDto;
 
 @CrossOrigin(origins = "*")
 @Controller
@@ -28,9 +30,12 @@ public class ModifyController {
 	BCryptPasswordEncoder passwordEncoder;
 	
 	@RequestMapping(value = "/api/v1/validatePw", method = RequestMethod.POST)
-	public @ResponseBody String validatePw(@RequestParam String id, String pw){
+	public @ResponseBody String validatePw(@RequestBody LoginDto valDtoReq){
+		String id = valDtoReq.getId();
+		System.out.println("아이디 : "+id);
+		String rawPw = valDtoReq.getPw();
 		String encPw = modifyDao.validate(id);
-		String rawPw = pw;
+		
 		if(rawPw == null) 
 			return "-1";
 		
